a84c90a0584a6c7044a780c0a3ba784b1a75215a
Gianluca Randazzo <fusillicode@gmail.com>
2023-02-20 09:45:25 +0100

    Setting up Auth...maybe 🤔

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use axum::extract::Query;
+use axum::routing::get;
+use axum::routing::Router;
+use serde::Deserialize;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(auth));
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+enum AuthResult {
+    Success(SuccessfulAuth),
+    Fail(FailedAuth),
+}
+
+#[derive(Debug, Deserialize)]
+struct SuccessfulAuth {
+    code: String,
+    scope: String,
+    state: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct FailedAuth {
+    error: String,
+    error_description: String,
+    state: String,
+}
+
+async fn auth(Query(auth_result): Query<AuthResult>) -> String {
+    dbg!(auth_result);
+    "foo".into()
 }
