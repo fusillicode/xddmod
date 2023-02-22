1d34806b528a79e5725df3e4b48da3f1b2b45c33
Gianluca Randazzo <fusillicode@gmail.com>
2023-02-22 08:33:13 +0100

    File token storage & moar stuff...

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use std::net::SocketAddr;
+use std::path::Path;
+use std::path::PathBuf;
 use std::sync::Arc;
 
+use axum::async_trait;
 use axum::extract::Query;
 use axum::extract::State;
 use axum::response::Redirect;
@@ -8,10 +12,19 @@ use axum::routing::Router;
 use config::Config;
 use config::Environment;
 use hyper::header::CONTENT_TYPE;
+use hyper::Body;
+use hyper::Client;
 use hyper::Request;
 use serde::Deserialize;
 use serde::Serialize;
 use sqlx::SqlitePool;
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
+use tokio::io::AsyncWriteExt;
+use twitch_irc::login::GetAccessTokenResponse;
+use twitch_irc::login::RefreshingLoginCredentials;
+use twitch_irc::login::TokenStorage;
+use twitch_irc::login::UserAccessToken;
 use url::Url;
 
 #[tokio::main]
@@ -23,60 +36,139 @@ async fn main() {
         .try_deserialize()
         .unwrap();
 
-    dbg!(&app_config);
-
     let db_pool = SqlitePool::connect(app_config.database_url.as_ref())
         .await
         .unwrap();
     sqlx::migrate!().run(&db_pool).await.unwrap();
 
+    let token_storage = TokenFileStorage::init(app_config.token_file_path.as_path())
+        .await
+        .unwrap();
+
+    let credentials = RefreshingLoginCredentials::init(
+        app_config.client_id.clone(),
+        app_config.client_secret.clone(),
+        token_storage.try_clone().await.unwrap(),
+    );
+
     let app_state = Arc::new(AppState {
-        db_pool,
-        config: app_config,
+        token_storage,
+        db_pool: db_pool.clone(),
+        config: app_config.clone(),
+        credentials,
     });
 
-    let app = Router::new().route("/", get(auth)).with_state(app_state);
+    let host = app_state.config.host.clone();
 
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    let app = Router::new()
+        .route("/", get(auth_callback))
+        .with_state(app_state);
+
+    axum::Server::bind(&Socket::try_from(&host).unwrap().into())
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
-async fn auth(
-    State(app_state): State<Arc<AppState>>,
+struct Socket(SocketAddr);
+
+impl TryFrom<&Url> for Socket {
+    type Error = anyhow::Error;
+
+    fn try_from(x: &Url) -> Result<Self, Self::Error> {
+        Ok(Self(
+            x.host_str()
+                .and_then(|h| x.port().map(|p| format!("{}:{}", h, p)))
+                .ok_or_else(|| anyhow::anyhow!("Error creating SocketAddr from Url {:?}", x))?
+                .parse::<SocketAddr>()?,
+        ))
+    }
+}
+
+impl From<Socket> for SocketAddr {
+    fn from(x: Socket) -> Self {
+        x.0
+    }
+}
+
+async fn auth_callback<T: TokenStorage>(
+    State(app_state): State<Arc<AppState<T>>>,
     Query(auth_result): Query<AuthResult>,
 ) -> Redirect {
     let SuccessfulAuth { code, .. } =
         Result::from(auth_result).unwrap_or_else(|failed_auth| panic!("{:?}", failed_auth));
 
-    Request::post::<&str>(app_state.config.twitch_oauth2_endpoint.as_ref())
+    let request = AuthRequest {
+        client_id: app_state.config.client_id.clone(),
+        client_secret: app_state.config.client_secret.clone(),
+        code,
+        grant_type: "authorization_code".into(),
+        redirect_uri: app_state.config.host.clone(),
+    };
+
+    let request = Request::post::<&str>(app_state.config.twitch_oauth2_endpoint.as_ref())
         .header(CONTENT_TYPE, "application/x-www-form-urlencoded")
-        .body(AuthRequest {
-            client_id: app_state.config.client_id.clone(),
-            client_secret: app_state.config.client_secret.clone(),
-            code,
-            grant_type: "authorization_code".into(),
-            redirect_uri: app_state.config.host.clone(),
-        })
+        .body(Body::from(serde_json::to_string(&request).unwrap()))
         .unwrap();
+
+    let response = Client::new().request(request).await.unwrap();
+    let body = serde_json::from_slice::<GetAccessTokenResponse>(
+        &hyper::body::to_bytes(response.into_body()).await.unwrap(),
+    )
+    .unwrap();
 
     Redirect::to("/start")
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 struct AppConfig {
     host: Url,
     database_url: Url,
+    token_file_path: PathBuf,
     client_id: String,
     client_secret: String,
     twitch_oauth2_endpoint: Url,
 }
 
 #[derive(Debug)]
-struct AppState {
+struct AppState<T: TokenStorage> {
+    token_storage: TokenFileStorage,
     db_pool: SqlitePool,
     config: AppConfig,
+    credentials: RefreshingLoginCredentials<T>,
+}
+
+#[derive(Debug)]
+struct TokenFileStorage(File);
+
+impl TokenFileStorage {
+    pub async fn init(file_path: &Path) -> std::io::Result<Self> {
+        file_path.try_exists()?;
+        Ok(Self(File::open(file_path).await?))
+    }
+
+    pub async fn try_clone(&self) -> std::io::Result<Self> {
+        Ok(Self(self.0.try_clone().await?))
+    }
+}
+
+#[async_trait]
+impl TokenStorage for TokenFileStorage {
+    type LoadError = std::io::Error;
+    type UpdateError = std::io::Error;
+
+    async fn load_token(&mut self) -> Result<UserAccessToken, Self::LoadError> {
+        let mut file_content = String::new();
+        self.0.read_to_string(&mut file_content).await?;
+        Ok(serde_json::from_str::<UserAccessToken>(&file_content).unwrap())
+    }
+
+    async fn update_token(&mut self, token: &UserAccessToken) -> Result<(), Self::UpdateError> {
+        self.0
+            .write_all(&serde_json::to_vec(&token).unwrap())
+            .await?;
+        Ok(())
+    }
 }
 
 #[derive(Debug, Deserialize)]
