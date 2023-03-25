5a44b5d2696b54c17f9754c15d10ddadaad9359a
Gianluca Randazzo <fusillicode@gmail.com>
2023-03-25 14:23:25 +0100

    ;(

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,24 @@
 use std::net::SocketAddr;
-use std::path::Path;
-use std::path::PathBuf;
 use std::sync::Arc;
 use std::time::Duration;
 
 use axum::async_trait;
+use axum::extract::Query;
 use axum::extract::State;
-use axum::response::Redirect;
 use axum::routing::get;
 use axum::routing::Router;
 use config::Config;
 use config::Environment;
 use serde::Deserialize;
 use sqlx::SqlitePool;
-use tokio::fs::File;
-use tokio::io::AsyncReadExt;
-use tokio::io::AsyncWriteExt;
+use tokio::sync::Mutex;
 use twitch_irc::login::GetAccessTokenResponse;
 use twitch_irc::login::RefreshingLoginCredentials;
 use twitch_irc::login::TokenStorage;
 use twitch_irc::login::UserAccessToken;
+use twitch_irc::TwitchIRCClient;
 use twitch_oauth2::tokens::UserTokenBuilder;
+use twitch_oauth2::TwitchToken;
 use url::Url;
 
 #[tokio::main]
@@ -37,34 +35,6 @@ async fn main() {
         .unwrap();
     sqlx::migrate!().run(&db_pool).await.unwrap();
 
-    let token_storage = TokenFileStorage::init(app_config.token_file_path.as_path())
-        .await
-        .unwrap();
-
-    let credentials = RefreshingLoginCredentials::init(
-        app_config.client_id.clone(),
-        app_config.client_secret.clone(),
-        token_storage.try_clone().await.unwrap(),
-    );
-
-    let app_state = Arc::new(AppState {
-        token_storage,
-        credentials,
-        db_pool: db_pool.clone(),
-        config: app_config.clone(),
-    });
-
-    let app = Router::new()
-        .route("/", get(auth_callback))
-        .with_state(app_state);
-
-    tokio::spawn(async move {
-        axum::Server::bind(&app_config.socket_addr)
-            .serve(app.into_make_service())
-            .await
-            .unwrap();
-    });
-
     let auth_callback_url = {
         let mut x = app_config.server_url.clone();
         x.set_path("auth");
@@ -75,33 +45,129 @@ async fn main() {
         app_config.client_id.clone(),
         app_config.client_secret.clone(),
         auth_callback_url,
-    );
-
-    user_token_builder.set_scopes(vec![
+    )
+    .set_scopes(vec![
         twitch_oauth2::Scope::ChatRead,
         twitch_oauth2::Scope::ChatEdit,
     ]);
 
     let (auth_url, _) = user_token_builder.generate_url();
 
-    println!("GO HERE {} AND THEN CONTINUE...", auth_url);
+    println!("GET THE FUCK HERE {}", auth_url);
 
-    std::io::stdin().read_line(String::new()).unwrap();
+    let app_state = Arc::new(AppState {
+        auth_response_step_1: Mutex::new(None),
+    });
 
-    while File::open("foo.txt").await.is_err() {
-        tokio::time::sleep(Duration::from_secs(2));
-        dbg!("WAIT");
+    let app = Router::new()
+        .route("/auth", get(auth_callback))
+        .with_state(app_state.clone());
+
+    tokio::spawn(async move {
+        axum::Server::bind(&app_config.socket_addr)
+            .serve(app.into_make_service())
+            .await
+            .unwrap();
+    });
+
+    loop {
+        if app_state.auth_response_step_1.lock().await.is_some() {
+            break;
+        }
+        tokio::time::sleep(Duration::from_secs(2)).await;
+        println!("WAITING...");
     }
 
-    dbg!("DONE");
+    let AuthResponseStep1 { code, state } =
+        app_state.auth_response_step_1.lock().await.clone().unwrap();
+    let client = reqwest::Client::new();
+
+    let user_token = user_token_builder
+        .get_user_token(&client, state.as_str(), code.as_str())
+        .await
+        .unwrap();
+
+    // let http_client = reqwest::Client::new();
+    // let response = http_client
+    //     .execute(ciccia.try_into().unwrap())
+    //     .await
+    //     .unwrap();
+    // let merda = axum::http::Response::builder()
+    //     .status(200)
+    //     .body(response.bytes().await.unwrap())
+    //     .unwrap();
+    // let twitch_response = TwitchTokenResponse::from_response(&merda).unwrap();
+    // dbg!(&twitch_response);
+
+    let get_access_token_response = GetAccessTokenResponse {
+        access_token: user_token.access_token.to_string(),
+        refresh_token: user_token.refresh_token.clone().unwrap().to_string(),
+        expires_in: Some(user_token.expires_in().as_secs()),
+    };
+
+    let custom_token_storage = CustomTokenStorage(get_access_token_response.into());
+
+    let credentials = RefreshingLoginCredentials::init(
+        app_config.client_id.clone(),
+        app_config.client_secret.clone(),
+        custom_token_storage,
+    );
+
+    let client_config = twitch_irc::ClientConfig::new_simple(credentials);
+    let (mut incoming_messages, client) = TwitchIRCClient::<
+        twitch_irc::SecureTCPTransport,
+        RefreshingLoginCredentials<CustomTokenStorage>,
+    >::new(client_config);
+
+    dbg!("MERDA");
+
+    // client.join("fusillicode".to_owned()).unwrap();
+    dbg!(client.get_channel_status("fusillicode".to_owned()).await);
+    client
+        .say("fusillicode".into(), "xdd".into())
+        .await
+        .unwrap();
+
+    let join_handle = tokio::spawn(async move {
+        while let Some(message) = incoming_messages.recv().await {
+            println!("Received message: {:?}", message);
+        }
+    });
+
+    join_handle.await.unwrap();
 }
 
-async fn auth_callback<T: TokenStorage>(
-    State(app_state): State<Arc<AppState<T>>>,
-    // Query(auth_result): Query<AuthResult>,
-) -> () {
-    let mut file = File::create("foo.txt").await.unwrap();
-    file.write_all(b"Hello, world!").await.unwrap();
+#[derive(Debug, Deserialize, Clone)]
+pub struct AuthResponseStep1 {
+    pub code: String,
+    pub state: String,
+}
+
+async fn auth_callback(
+    State(app_state): State<Arc<AppState>>,
+    Query(auth_response_step_1): Query<AuthResponseStep1>,
+) {
+    dbg!(&auth_response_step_1);
+    let mut guard = app_state.auth_response_step_1.lock().await;
+    *guard = Some(auth_response_step_1.clone());
+}
+
+#[derive(Debug)]
+struct CustomTokenStorage(UserAccessToken);
+
+#[async_trait]
+impl TokenStorage for CustomTokenStorage {
+    type LoadError = std::io::Error;
+    type UpdateError = std::io::Error;
+
+    async fn load_token(&mut self) -> Result<UserAccessToken, Self::LoadError> {
+        Ok(self.0.clone())
+    }
+
+    async fn update_token(&mut self, token: &UserAccessToken) -> Result<(), Self::UpdateError> {
+        self.0 = token.clone();
+        Ok(())
+    }
 }
 
 #[derive(Debug, Deserialize, Clone)]
@@ -109,47 +175,10 @@ struct AppConfig {
     socket_addr: SocketAddr,
     server_url: Url,
     database_url: Url,
-    token_file_path: PathBuf,
     client_id: String,
     client_secret: String,
 }
 
-struct AppState<T: TokenStorage> {
-    token_storage: TokenFileStorage,
-    credentials: RefreshingLoginCredentials<T>,
-    db_pool: SqlitePool,
-    config: AppConfig,
-}
-
-#[derive(Debug)]
-struct TokenFileStorage(File);
-
-impl TokenFileStorage {
-    pub async fn init(file_path: &Path) -> std::io::Result<Self> {
-        file_path.try_exists()?;
-        Ok(Self(File::open(file_path).await?))
-    }
-
-    pub async fn try_clone(&self) -> std::io::Result<Self> {
-        Ok(Self(self.0.try_clone().await?))
-    }
-}
-
-#[async_trait]
-impl TokenStorage for TokenFileStorage {
-    type LoadError = std::io::Error;
-    type UpdateError = std::io::Error;
-
-    async fn load_token(&mut self) -> Result<UserAccessToken, Self::LoadError> {
-        let mut file_content = String::new();
-        self.0.read_to_string(&mut file_content).await?;
-        Ok(serde_json::from_str::<UserAccessToken>(&file_content).unwrap())
-    }
-
-    async fn update_token(&mut self, token: &UserAccessToken) -> Result<(), Self::UpdateError> {
-        self.0
-            .write_all(&serde_json::to_vec(&token).unwrap())
-            .await?;
-        Ok(())
-    }
+struct AppState {
+    auth_response_step_1: Mutex<Option<AuthResponseStep1>>,
 }
